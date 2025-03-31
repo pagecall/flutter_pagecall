be7ce5eded8014176753cb255b9862a382ebf779
Jaeseong Seo <07js23@gmail.com>
2025-03-31 21:07:02 +0900

    fix: typo

--- example/android/app/src/main/kotlin/com/pagecall/pagecall_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/pagecall/pagecall_flutter_example/MainActivity.kt
@@ -5,12 +5,4 @@ import com.pagecall.flutter_pagecall.FlutterPagecallView
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        for (instance in FlutterPagecallView.instances) {
-            if(instance.handleKeyDownEvent(keyCode, event)) {
-               return true
-            }
-        }
-        return super.onKeyDown(keyCode, event)
-    }
 }
